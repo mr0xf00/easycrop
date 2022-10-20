c6c626ddb814c25169c2e2cf5c0ece734c3d2ec0
mr0xf00 <52595099+mr0xf00@users.noreply.github.com>
2022-10-20 22:08:23 +0100

    Add readme

--- sample/src/main/java/com/mr0xf00/easycrop/ui/DemoContent.kt
+++ sample/src/main/java/com/mr0xf00/easycrop/ui/DemoContent.kt
@@ -1,17 +1,14 @@
 package com.mr0xf00.easycrop.ui
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.unit.dp
 import com.mr0xf00.easycrop.CropState
 import com.mr0xf00.easycrop.CropperLoading
-import com.mr0xf00.easycrop.CropperStyle
 import com.mr0xf00.easycrop.ui.theme.EasyCropTheme
 
 @Composable
@@ -24,14 +21,7 @@ fun DemoContent(
 ) {
     if (cropState != null) {
         EasyCropTheme(darkTheme = true) {
-            ImageCropperDialog(
-                state = cropState,
-                style = CropperStyle(
-                    overlay = Color.Red.copy(alpha = .5f),
-                    autoZoom = false,
-                    guidelines = null,
-                )
-            )
+            ImageCropperDialog(state = cropState)
         }
     }
     if (cropState == null && loadingStatus != null) {
